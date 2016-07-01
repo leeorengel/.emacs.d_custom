65b89e34363e8d3fbb2edc8f32b31306e0ce6753
Leeor Engel <leeorengel@gmail.com>
2016-07-01 15:06:41 -0400

    Initial setup

---
+++ customizations/00-global.el
@@ -0,0 +1,11 @@
+;;; global --- Summary
+;;; Commentary:
+;;; Code:
+
+;;; Fancier dired display
+(require 'dired-details+)
+
+(ido-vertical-mode)
+
+(provide '00-global)
+;;; 00-global.el ends here
---
+++ init.el
@@ -0,0 +1,8 @@
+(require 'cask "/usr/local/Cellar/cask/0.7.4/cask.el")
+(cask-initialize)
+(require 'pallet)
+(pallet-mode t)
+
+(mapc 'load (directory-files "~/.emacs.d/customizations" t "^[0-9]+.*\.el$"))
+
+(provide 'init)
