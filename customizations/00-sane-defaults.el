d03723e13892be467ff8a7810d314ac0b94f2bef
Leeor Engel <leeorengel@gmail.com>
2016-07-02 00:01:05 -0400

    smooth-scrolling

--- customizations/00-sane-defaults.el
+++ customizations/00-sane-defaults.el
@@ -72,9 +72,6 @@
 ;; Don't break lines for me, please
 (setq-default truncate-lines t)
 
-;; Keep cursor away from edges when scrolling up/down
-(require 'smooth-scrolling)
-
 ;; Allow recursive minibuffers
 (setq enable-recursive-minibuffers t)
 
