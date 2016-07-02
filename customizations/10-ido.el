e309818adb1f976d6cc829005394411af98cc4e8
Leeor Engel <leeorengel@gmail.com>
2016-07-01 22:50:24 -0400

    added ido config

---
+++ customizations/10-ido.el
@@ -0,0 +1,47 @@
+;; Interactively Do Things
+
+(require 'ido)
+(ido-mode t)
+(setq ido-enable-prefix nil
+      ido-enable-flex-matching t
+      ido-case-fold nil
+      ido-auto-merge-work-directories-length -1
+      ido-create-new-buffer 'always
+      ido-use-filename-at-point nil
+      ido-max-prospects 10)
+
+(require 'flx-ido)
+(flx-ido-mode 1)
+;; disable ido faces to see flx highlights.
+(setq ido-use-faces nil)
+
+;; flx-ido looks better with ido-vertical-mode
+(require 'ido-vertical-mode)
+(ido-vertical-mode)
+
+;; C-n/p is more intuitive in vertical layout
+(setq ido-vertical-define-keys 'C-n-C-p-up-down-left-right)
+
+;; Always rescan buffer for imenu
+(set-default 'imenu-auto-rescan t)
+
+;; Ido at point (C-,)
+(require 'ido-at-point)
+(ido-at-point-mode)
+
+;; Use ido everywhere
+(require 'ido-ubiquitous)
+(ido-ubiquitous-mode 1)
+
+;; Fix ido-ubiquitous for newer packages
+(defmacro ido-ubiquitous-use-new-completing-read (cmd package)
+  `(eval-after-load ,package
+     '(defadvice ,cmd (around ido-ubiquitous-new activate)
+        (let ((ido-ubiquitous-enable-compatibility nil))
+          ad-do-it))))
+
+(ido-ubiquitous-use-new-completing-read webjump 'webjump)
+(ido-ubiquitous-use-new-completing-read yas-expand 'yasnippet)
+(ido-ubiquitous-use-new-completing-read yas-visit-snippet-file 'yasnippet)
+
+(provide 'ido)
