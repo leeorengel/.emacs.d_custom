9b1a120c94927ed0070459d044e626f8f56daa76
Leeor Engel <leeorengel@gmail.com>
2016-07-03 16:21:34 -0400

    smartparens config update

--- customizations/10-smartparens.el
+++ customizations/10-smartparens.el
@@ -6,14 +6,9 @@
 (add-hook 'emacs-lisp-mode-hook #'smartparens-strict-mode)
 (add-hook 'cider-repl-mode-hook #'smartparens-strict-mode)
 
-;; Use the keybindings from paredit
-(setq sp-base-key-bindings 'paredit)
-
-;; Bind the additional smart parens keybindings
-(sp-use-paredit-bindings)
+(sp-use-smartparens-bindings)
 
 (setq sp-autoskip-closing-pair 'always)
-
 (setq sp-hybrid-kill-entire-symbol nil)
 
 (provide '10-smartparens)
